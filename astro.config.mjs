df33fa2c14de9e9c8d12fb7f236ab02f6580e51f
SvZ <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-05-13 08:59:10 +0200

    Install Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kodnord.netlify.app/",
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [sitemap(), mdx()],
+  adapter: netlify(),
 });
