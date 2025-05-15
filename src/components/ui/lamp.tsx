e76508f0b34db9de7fc37b9122ac3ae4c90bd633
SvZ <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-05-15 19:25:26 +0200

    Update some styling & cleanup

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -16,7 +16,7 @@ export const LampContainer = ({
         className
       )}
     >
-      <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0">
+      <div className="relative flex w-full flex-1 scale-y-120 items-center justify-center isolate z-0">
         {/* Left side flicker beam */}
         <div
           style={{
@@ -26,8 +26,8 @@ export const LampContainer = ({
           }}
           className="absolute inset-auto right-1/2 overflow-visible bg-gradient-conic from-purple-400 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top] animate-flicker"
         >
-          <div className="absolute w-full left-0 bg-background h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div className="absolute w-40 h-full left-0 bg-background bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+          <div className="absolute w-full left-0 bg-transparent h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-40 h-full left-0 bg-transparent bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
         </div>
 
         {/* Right side flicker beam */}
@@ -39,12 +39,12 @@ export const LampContainer = ({
           }}
           className="absolute inset-auto left-1/2 bg-gradient-conic from-transparent via-transparent to-purple-400 text-white [--conic-position:from_290deg_at_center_top] animate-flicker"
         >
-          <div className="absolute w-40 h-full right-0 bg-background bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-          <div className="absolute w-full right-0 bg-background h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-40 h-full right-0 bg-transparent bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+          <div className="absolute w-full right-0 bg-transparent h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </div>
 
         {/* Shadow beam */}
-        <div className="absolute top-1/2 translate-y-12 scale-x-150 w-full h-[12rem] bg-background blur-2xl" />
+        <div className="absolute top-1/2 translate-y-12 scale-x-150 w-full h-[12rem] bg-transparent blur-2xl" />
 
         {/* Blurred overlay */}
         <div className="absolute top-1/2 z-50 h-[12rem] w-full bg-transparent opacity-10 backdrop-blur-md" />
